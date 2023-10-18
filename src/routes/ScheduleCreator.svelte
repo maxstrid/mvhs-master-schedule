40c1735c99e823c9a8ed07383c05e031ba6e8be3
Maxwell Henderson <maxwell.henderson@mailbox.org>
2023-10-18 14:26:17 -0700

    Add basic addition of elements to a schedule
    Signed-off-by: Maxwell Henderson <maxwell.henderson@mailbox.org>

--- src/routes/ScheduleCreator.svelte
+++ src/routes/ScheduleCreator.svelte
@@ -1,7 +1,39 @@
 <script lang="ts">
 	export let period: number;
+
+	let classLength = 0;
+
+	function addClass() {
+		classLength += 1;
+	}
 </script>
 
-<div>
+<div id="schedule-row">
 	Period {period}
+
+	{#each Array(classLength) as _}
+		<select>
+			<option>a</option>
+		</select>
+	{/each}
+
+	<button id="add-class-button" on:click={addClass}> + </button>
 </div>
+
+<style>
+	#schedule-row {
+		margin: 1em;
+	}
+
+	#add-class-button {
+		border: 2px solid #000000;
+		border-radius: 0.75em;
+		padding: 0.25em 0.5em 0.25em 0.5em;
+		font-size: 1em;
+		background: var(--accent-color);
+	}
+
+	#add-class-button:hover {
+		filter: brightness(90%);
+	}
+</style>
