d8955d06d13f70447d85a23b00bfeb0004a0a3f1
Maxwell Henderson <maxwell.henderson@mailbox.org>
2023-11-23 12:02:01 -0800

    Add basic example of connecting to flask
    Signed-off-by: Maxwell Henderson <maxwell.henderson@mailbox.org>

--- flask/server.py
+++ flask/server.py
@@ -8,5 +8,10 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
+@app.route("/test")
+def test():
+    return {"info": "This is test data"}
+
+
 if __name__ == "__main__":
     app.run(debug=True)
