078759463d2f6ab912741432a93f67acfd80eaeb
Maxwell Henderson <maxwell.henderson@mailbox.org>
2023-11-27 22:12:33 -0800

    Add vite template setup
    Signed-off-by: Maxwell Henderson <maxwell.henderson@mailbox.org>

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,9 @@
-import { useState, useCallback, useEffect } from 'react';
-
-import './App.css';
-
-import SchedulePeriod from './SchedulePeriod';
-
-type SchedulePeriodResponse = {
-    period: number;
-    classes: string[];
-}
-
-type ScheduleResponse = {
-    body: SchedulePeriodResponse[];
-}
-
 function App() {
-    const [data, setData] = useState<ScheduleResponse | null>(null);
-
-    useEffect(() => {
-        fetch("/generate_schedule").then(
-            res => res.json()
-        ).then(
-            data => {
-                setData(data)
-            }
-        )
-    }, []);
-
-
-    const regenerate = useCallback(() => {
-        fetch("/generate_schedule").then(
-            res => res.json()
-        ).then(
-            data => {
-                setData(data)
-            }
-        )
-    }, []);
-
     return (
-        <div className='flex flex-col m-auto'>
-            <div className='m-auto mb-20'>
-                {(data == null) ? (
-                    <h1>Loading...</h1>
-                ) : (
-                    data?.body.map((period: SchedulePeriodResponse, i: number) => {
-                        return <SchedulePeriod key={i} classes={period.classes} period={period.period} />
-                    })
-                )}
-            </div>
-
-            <div className='m-auto'>
-                <button className='btn' onClick={regenerate} >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-                    </svg>
-
-                    <span>Regenerate</span>
-                </button>
-                <button className='btn'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
-                    </svg>
-
-                    <span>Import</span>
-                </button>
-                <button className='btn'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
-                    </svg>
-
-                    <span>Export</span>
-                </button>
-            </div>
-        </div >
-    );
+        <>
+            <h1>Hello, World</h1>
+        </>
+    )
 }
 
-export default App;
+export default App
