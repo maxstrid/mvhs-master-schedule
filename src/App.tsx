7f3212c4b6e043069fc6f1ac0c25a60dff2d3aec
Maxwell Henderson <mxwhenderson@gmail.com>
2023-12-16 07:40:34 -0800

    Switch to using react heroicons
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState, useCallback, useEffect } from 'react';
 import './App.css';
 
 import SchedulePeriod from './SchedulePeriod';
+import { ArrowDownTrayIcon, ArrowPathIcon, ArrowUpTrayIcon } from '@heroicons/react/20/solid';
 
 type SchedulePeriodResponse = {
     period: number;
@@ -51,24 +52,15 @@ function App() {
 
             <div className='m-auto'>
                 <button className='btn' onClick={regenerate} >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-                    </svg>
-
+                    <ArrowPathIcon className='h-6 w-6' />
                     <span>Regenerate</span>
                 </button>
                 <button className='btn'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
-                    </svg>
-
+                    <ArrowUpTrayIcon className='h-6 w-6' />
                     <span>Import</span>
                 </button>
                 <button className='btn'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
-                    </svg>
-
+                    <ArrowDownTrayIcon className='h-6 w-6' />
                     <span>Export</span>
                 </button>
             </div>
