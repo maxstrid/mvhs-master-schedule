2d0b4fe47338b3789a8478c2798cf973ac784bbb
FordoA455 <milobylin@gmail.com>
2024-03-04 12:12:04 -0800

    WIP conflict matrix code
    Signed-off-by: FordoA455 <milobylin@gmail.com>

---
+++ flask/conflict_calc.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+# fixes issues with relative paths and running file from different locations
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, 'conflict_matrix.txt')
+
+df = pd.DataFrame()
+df["courseNumber1"] = ""
+df["courseName1"] = ""
+df["courseRequests1"] = 0
+df["courseNumber2"] = ""
+df["courseName2"] = ""
+df["courseRequests2"] = 0
+df["courseNumber3"] = ""
+df["courseName3"] = ""
+df["courseRequests3"] = ""
+
+with open(filename, 'r') as file:
+  for line in file:
+    print(line)
