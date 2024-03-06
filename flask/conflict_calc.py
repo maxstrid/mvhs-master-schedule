09cd04c91647c4be5ee70bb603e3ff7441b1b2e3
FordoA455 <milobylin@gmail.com>
2024-03-06 11:54:21 -0800

    Adding main function
    Signed-off-by: FordoA455 <milobylin@gmail.com>

--- flask/conflict_calc.py
+++ flask/conflict_calc.py
@@ -1,21 +1,26 @@
 import pandas as pd
 import os
 
-# fixes issues with relative paths and running file from different locations
-dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, 'conflict_matrix.txt')
 
-df = pd.DataFrame()
-df["courseNumber1"] = ""
-df["courseName1"] = ""
-df["courseRequests1"] = 0
-df["courseNumber2"] = ""
-df["courseName2"] = ""
-df["courseRequests2"] = 0
-df["courseNumber3"] = ""
-df["courseName3"] = ""
-df["courseRequests3"] = ""
+def main():
+  # fixes issues with relative paths and running file from different locations
+  dirname = os.path.dirname(__file__)
+  filename = os.path.join(dirname, 'conflict_matrix.txt')
 
-with open(filename, 'r') as file:
-  for line in file:
-    print(line)
+  df = pd.DataFrame()
+  df["courseNumber1"] = ""
+  df["courseName1"] = ""
+  df["courseRequests1"] = 0
+  df["courseNumber2"] = ""
+  df["courseName2"] = ""
+  df["courseRequests2"] = 0
+  df["courseNumber3"] = ""
+  df["courseName3"] = ""
+  df["courseRequests3"] = ""
+
+  with open(filename, 'r') as file:
+    for line in file:
+      print(line)
+
+if __name__ == "__main__":
+  main()
