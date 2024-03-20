c9f9b9d4ed4ac0b315fbea911d9c2672c4508c77
Maxwell Henderson <mxwhenderson@gmail.com>
2024-03-20 00:32:31 -0700

    Add a different test case
    The way we'll want to actually test this is not with 1 predictable
    output but with a threshold of total conflicts. Its also likely we may
    want our algorithm to run more than once and take the answer with the
    lowest conflicts.
    
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- flask/schedule_generator_test.py
+++ flask/schedule_generator_test.py
@@ -19,5 +19,37 @@ class TestScheduleGenerator(unittest.TestCase):
             1: {"b", "a"}
         })
 
+    def test_bigger_schedule(self):
+        graph = Graph()
+
+        graph.add("AP Calc BC")
+        graph.add("Advanced CS")
+        graph.add("AP Physics I")
+        graph.add("Statistics")
+        graph.add("Chemistry H")
+        graph.add("American Lit H")
+
+        graph.add_edge("AP Calc BC", "Advanced CS", 2)
+        graph.add_edge("AP Calc BC", "AP Physics I", 18)
+        graph.add_edge("AP Calc BC", "Statistics", 3)
+        graph.add_edge("AP Calc BC", "Chemistry H", 2)
+        graph.add_edge("AP Calc BC", "American Lit H", 15)
+
+        graph.add_edge("Advanced CS", "AP Physics I", 37)
+        graph.add_edge("Advanced CS", "Statistics", 1)
+        graph.add_edge("Advanced CS", "Chemistry H", 5)
+        graph.add_edge("Advanced CS", "American Lit H", 22)
+
+        graph.add_edge("AP Physics I", "Statistics", 3)
+        graph.add_edge("AP Physics I", "Chemistry H", 2)
+        graph.add_edge("AP Physics I", "American Lit H", 64)
+
+        graph.add_edge("Statistics", "Chemistry H", 1)
+        graph.add_edge("Statistics", "American Lit H", 2)
+
+        graph.add_edge("Chemistry H", "American Lit H", 1)
+
+        print(assign_classes(graph))
+
 if __name__ == '__main__':
     unittest.main()
